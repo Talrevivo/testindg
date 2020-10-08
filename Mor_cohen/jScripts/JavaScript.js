abbf75eeb3cbfab4bd8c88aa8e8a3c864a6e63a1
Talrevivo <37411352+Talrevivo@users.noreply.github.com>
2020-10-08 14:01:21 +0300

    Add files via upload

---
+++ Mor_cohen/jScripts/JavaScript.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+    });
+
+    $(window).on("load", function () {
+        $(window).scroll(function () {
+            var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+            $(".fade").each(function () {
+                /* Check the location of each desired element */
+                var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+                /* If the element is completely within bounds of the window, fade it in */
+                if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+                    if ($(this).css("opacity") == 0) { $(this).fadeTo(500, 1); }
+                } else { //object goes out of view (scrolling up)
+                    if ($(this).css("opacity") == 1) { $(this).fadeTo(500, 0); }
+                }
+            });
+        }).scroll(); //invoke scroll-handler on page-load
+    });
+
+    $("#menu-img").click(function () {
+        $(".nB").show();
+        $("#x").show();
+        $(".nB").addClass("navclick-design");
+    });
+
+    $("#x").click(function () {
+        $(".nB").hide();
+        $(".nB").addClass("navclick-design");
+        location.reload();
+    });
+
+});
